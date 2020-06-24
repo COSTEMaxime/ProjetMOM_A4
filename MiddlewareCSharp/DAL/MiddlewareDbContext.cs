019ad999ad9f85152ad0b4c471cc0ee05eb40b07
Coste Maxime <maxime.coste2@hotmail.fr>
2020-06-24 23:46:45 +0200

    Added data access layer and updated README.md

---
+++ MiddlewareCSharp/DAL/MiddlewareDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class MiddlewareDbContext : DbContext
+    {
+        public MiddlewareDbContext() : base("MiddlewareDbContext")
+        {
+        }
+
+        public DbSet<ServiceEntity> Services { get; set; }
+        public DbSet<UserGroupEntity> UserGroups { get; set; }
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<UserGroupServiceEntity> UserGroupServices { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations.Add(new UserGroupConfiguration());
+            modelBuilder.Configurations.Add(new UserConfiguration());
+            modelBuilder.Configurations.Add(new ServiceConfiguration());
+            modelBuilder.Configurations.Add(new UserGroupServiceConfiguration());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ProjetMOM_A4
+
+# Install Middleware C#
+## Database
+- Create a database (preferably using SQL Server) named `db_middleware`.
+- Open VS then open the Package Manager Console for the `DAL` project
+- Type `Update-Database`
