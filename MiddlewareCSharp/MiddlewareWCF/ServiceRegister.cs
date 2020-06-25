7b223fcf7340b0bdbdccdaff404c9c1f587809b4
Coste Maxime <maxime.coste2@hotmail.fr>
2020-06-26 00:36:44 +0200

    Added services documentation

--- MiddlewareCSharp/MiddlewareWCF/ServiceRegister.cs
+++ MiddlewareCSharp/MiddlewareWCF/ServiceRegister.cs
@@ -9,6 +9,15 @@ namespace MiddlewareWCF
 {
     class ServiceRegister : IService
     {
+        /*
+         * Input Data Layout :
+         *      - data[0] : login (string)
+         *      - data[1] : password (string)
+         *      - data[2] : email (string)
+         *      - data[3] : groups (List<string>)
+         *
+         * Output Data Layout :
+         */
         public Message ExecuteService(Message message)
         {
             return BusinessAccessLayer.Dispatch(message);
