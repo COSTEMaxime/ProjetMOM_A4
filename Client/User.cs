3ca7df0a1e5f03a986e504e478d4fb0b67200a2c
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2020-06-25 13:50:42 +0200

    Added model classes

---
+++ Client/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    class User {
+        
+        public User() {
+            username = "";
+            password = "";
+        }
+
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
