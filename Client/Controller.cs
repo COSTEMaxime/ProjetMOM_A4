4332e7a12a9ec99b43120672bf95df6712eedcb1
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2020-06-25 13:51:52 +0200

    Added Controller classes

---
+++ Client/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    class Controller {
+
+        private void login() {
+
+        }
+
+        private void logout() {
+
+        }
+
+        private void register() {
+
+        }
+
+        private void decodeFile() {
+
+        }
+    }
+}
---
+++ Client/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    class Login : IAction {
+        public void carryOut() {
+            throw new NotImplementedException();
+        }
+    }
+}
