323c5607e02f2b1be1c71fd9900507e9e656ae9e
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2020-06-25 14:18:30 +0200

    Added messenger

---
+++ Client/LoginMessenger.cs
@@ -0,0 +1,14 @@
+﻿using Client.MiddlewareService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    class LoginMessenger : IMessenger {
+        public Message writeMessage(ApplicationInfo info, object[] data) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Client/LogoutMessenger.cs
@@ -0,0 +1,14 @@
+﻿using Client.MiddlewareService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    class LogoutMessenger : IMessenger {
+        public Message writeMessage(ApplicationInfo info, object[] data) {
+            throw new NotImplementedException();
+        }
+    }
+}
