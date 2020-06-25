77798cd485a551881d21c900150c9657f0ad80a2
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2020-06-25 13:58:19 +0200

    Added some project config

---
+++ Client/IMessageWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.MiddlewareService;
+
+namespace Client {
+    interface IMessageWriter {
+        Message generateMessage(ApplicationInfo info, object[] data);
+    }
+}
--- Client/MainView.cs
+++ Client/MainView.cs
@@ -10,12 +10,41 @@ using System.Windows.Forms;
 
 namespace Client {
     public partial class MainView : Form {
+
+        User user;
+
         public MainView() {
             InitializeComponent();
+            user = new User();
         }
 
         private void systemFilesTreeView_AfterSelect(object sender, TreeViewEventArgs e) {
 
+        }
+
+        private void loginButton_Click(object sender, EventArgs e) {
+            if(usernameTextBox.Text != "" && passwordTextBox.Text != "") {
+
+                // Update Model
+                user.username = usernameTextBox.Text;
+                user.password = passwordTextBox.Text;
+
+                // Generate Message
+                MiddlewareService.Message msg = new MiddlewareService.Message();
+                msg.operationName = "login";
+                msg.info = "LET ME IIIIN !!!";
+                msg.data = new Object[1];
+                msg.data.Append(user);
+
+                // Send to server
+                MiddlewareService.ServiceEntryPointClient client = new MiddlewareService.ServiceEntryPointClient();
+                client.AccessService(msg);
+
+                consoleTextBox.AppendText("Sent message do server.\n");
+                
+            } else {
+                consoleTextBox.AppendText("Enter a correct username or password.\n");
+            }
         }
     }
 }
