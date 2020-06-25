4dfb11e26520f557fa906a169600eab86d23ba39
NHURTEVENT <nicolas.hurtevent@viacesi.fr>
2020-06-25 13:41:36 +0200

    fixed builder DP

--- Java EE platform/checkerFacade-ejb/src/main/java/com/checker/messagemgmt/contract/MSG.java
+++ Java EE platform/checkerFacade-ejb/src/main/java/com/checker/messagemgmt/contract/MSG.java
@@ -5,59 +5,171 @@
  */
 package com.checker.messagemgmt.contract;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author Nico
  */
+@XmlRootElement
+    @XmlAccessorType(XmlAccessType.FIELD)
 public class MSG {
+    @XmlAttribute
     private boolean opStatus;
+    @XmlAttribute
     private String info;
+    @XmlAttribute
     private Object[] data;
+    @XmlAttribute
     private String operationName;
+    @XmlAttribute
     private String appToken;
+    @XmlAttribute
     private String userToken;
+    @XmlAttribute
     private String appVersion;
+    @XmlAttribute
     private String operationVersion;
-    
-    public MSG() {
-        
+
+    public static class MSGBuilder {
+
+        private boolean opStatus;
+        private String info;
+        private Object[] data;
+        private String operationName;
+        private String appToken;
+        private String userToken;
+        private String appVersion;
+        private String operationVersion;
+
+        public MSGBuilder() {
+
+        }
+
+        public boolean isOpStatus() {
+            return opStatus;
+        }
+
+        public MSGBuilder setOpStatus(boolean opStatus) {
+            this.opStatus = opStatus;
+            return this;
+        }
+
+        public String getInfo() {
+            return info;
+        }
+
+        public MSGBuilder setInfo(String info) {
+            this.info = info;
+            return this;
+        }
+
+        public Object[] getData() {
+            return data;
+        }
+
+        public MSGBuilder setData(Object[] data) {
+            this.data = data;
+            return this;
+        }
+
+        public String getOperationName() {
+            return operationName;
+        }
+
+        public MSGBuilder setOperationName(String operationName) {
+            this.operationName = operationName;
+            return this;
+        }
+
+        public String getAppToken() {
+            return appToken;
+        }
+
+        public MSGBuilder setAppToken(String appToken) {
+            this.appToken = appToken;
+            return this;
+        }
+
+        public String getUserToken() {
+            return userToken;
+        }
+
+        public MSGBuilder setUserToken(String userToken) {
+            this.userToken = userToken;
+            return this;
+        }
+
+        public String getAppVersion() {
+            return appVersion;
+        }
+
+        public MSGBuilder setAppVersion(String appVersion) {
+            this.appVersion = appVersion;
+            return this;
+        }
+
+        public String getOperationVersion() {
+            return operationVersion;
+        }
+
+        public MSGBuilder setOperationVersion(String operationVersion) {
+            this.operationVersion = operationVersion;
+            return this;
+        }
+
+        public MSG build() {
+            MSG msg = new MSG();
+            msg.opStatus = this.opStatus;
+            msg.info = this.info;
+            msg.data = this.data;
+            msg.operationName = this.operationName;
+            msg.appToken = this.appToken;
+            msg.userToken = this.userToken;
+            msg.appVersion = this.appVersion;
+            msg.operationVersion = this.operationVersion;
+            return msg;
+        }
+    }
+
+    private MSG() {
+
+    }
+
+    public boolean isOpStatus() {
+        return opStatus;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public Object[] getData() {
+        return data;
+    }
+
+    public String getOperationName() {
+        return operationName;
+    }
+
+    public String getAppToken() {
+        return appToken;
+    }
+
+    public String getUserToken() {
+        return userToken;
+    }
+
+    public String getAppVersion() {
+        return appVersion;
+    }
+
+    public String getOperationVersion() {
+        return operationVersion;
     }
     
-    public MSG addInfo(String info) {
-        this.info = info;
-        return this;
-    }
     
-    public MSG addOperationName(String operationName) {
-        this.operationName = operationName;
-        return this;
-    }
-    
-    public MSG addData(Object[] data) {
-        this.data = data;
-        return this;
-    }
-    
-    public MSG addAppToken(String appToken) {
-        this.appToken = appToken;
-        return this;
-    }
-    
-    public MSG addUserToken(String userToken) {
-        this.userToken = userToken;
-        return this;
-    }
-    
-    public MSG build() {
-        MSG msg = new MSG();
-        msg.opStatus = this.opStatus;
-        msg.info = this.info;
-        msg.data = this.data;
-        msg.operationName = this.operationName;
-        msg.appToken = this.appToken;
-        msg.userToken = this.userToken;
-        msg.appVersion = this.appVersion;
-        msg.operationVersion = this.operationVersion;
-        return msg;
-    }
 }
